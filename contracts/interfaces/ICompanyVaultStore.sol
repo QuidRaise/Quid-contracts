f38dbc1b6e42b537803bf65903728086938cfe15
Chidelu <emekanweke604@gmail.com>
2021-10-09 23:06:47 +0100

    Declared company vault interface

---
+++ contracts/interfaces/ICompanyVaultStore.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.0;
+
+interface ICompanyVaultStore {
+    function getCompanyTokenBalance(uint companyId) external returns (uint);
+
+    function getCompanyVaultBalance(uint companyId,address tokenContractAddress) external returns (uint);
+
+    function updateCompanyTokenBalance(uint companyId, uint amount) external;
+
+    function updateCompanyVaultBalance(uint companyId, address tokenContractAddress, uint amount) external;
+}
